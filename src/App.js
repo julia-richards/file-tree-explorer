abba67318bc1644b12bcb31aca8f1aebf8cd9a2b
julia-richards <julia.m.richards@gmail.com>
2021-03-08 18:04:23 -0700

    styles and nests files in folder component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 function App() {
   return (
     <div>
-      <Folder name="Desktop" />
+      <Folder name="Desktop">
+        <File name="Dogs.jpg" />
+        <File name="Cats.jpg" />
+      </Folder>
       <Folder name="Applications" />
     </div>
   );
 }
 
 const Folder = props => {
-  return <h4>{props.name}</h4>;
+  return (
+    <div style={{ border: "2px solid pink", margin: "5px" }}>
+      {props.name}
+      <div style={{ marginLeft: "17px" }}>{props.children}</div>
+    </div>
+  );
 };
 
+const File = props => {
+  return <div>{props.name}</div>;
+};
 export default App;
