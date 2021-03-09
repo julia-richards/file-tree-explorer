324e0cf5d0c8d842656320bbb09890649c248f79
julia-richards <julia.m.richards@gmail.com>
2021-03-08 18:37:05 -0700

    adds isOpen slice of state and adds icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React, { useState } from "react";
+
 function App() {
   return (
     <div>
       <Folder name="Desktop">
+        <Folder name="Music">
+          <File name="all_star.mp4" />
+          <File name="falling-water.mp4" />
+        </Folder>
         <File name="Dogs.jpg" />
-        <File name="Cats.jpg" />
+        <File name="Cats.png" />
       </Folder>
       <Folder name="Applications" />
     </div>
@@ -11,15 +17,37 @@ function App() {
 }
 
 const Folder = props => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { name, children } = props;
+  const direction = isOpen ? "down" : "right";
+
+  const handleClick = () => setIsOpen(!isOpen);
+
   return (
-    <div style={{ border: "2px solid pink", margin: "5px" }}>
-      {props.name}
-      <div style={{ marginLeft: "17px" }}>{props.children}</div>
+    <div style={{ margin: "5px" }}>
+      <span onClick={handleClick}>
+        <i className="blue folder icon"></i>
+        <i className={`caret ${direction} icon`}></i>
+      </span>
+      {name}
+      <div style={{ marginLeft: "17px" }}>{isOpen ? children : null}</div>
     </div>
   );
 };
 
 const File = props => {
-  return <div>{props.name}</div>;
+  const { name } = props;
+  const fileExtension = name.split(".")[1];
+  const fileIcons = {
+    mp4: "headphones",
+    jpg: "file image",
+    png: "file image outline",
+  };
+  return (
+    <div>
+      <i className={`${fileIcons[fileExtension]} icon`}></i>
+      {name}
+    </div>
+  );
 };
 export default App;
